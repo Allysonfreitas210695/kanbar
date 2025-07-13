507c93a5f72690f2752801a5b527d8eb8ed51c1f
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-07-13 19:05:37 -0300

    build: instalando e configurando o eslint + prettier

--- kanbar-front/src/App.tsx
+++ kanbar-front/src/App.tsx
@@ -1,19 +1,23 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Button } from './components/ui/Button'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Button } from './components/ui/Button';
 function App() {
-
-
   return (
     <>
-<BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1 className='text-white'><Button>Home</Button></h1>}/>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <h1 className="text-white">
+                <Button>Home</Button>
+              </h1>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
