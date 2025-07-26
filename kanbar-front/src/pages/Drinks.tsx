246dba1c1b53b2b34b4eefbb743ab5d8cff920ce
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-07-26 19:46:34 -0300

    feat: adicionando o header da page drink

--- kanbar-front/src/pages/Drinks.tsx
+++ kanbar-front/src/pages/Drinks.tsx
@@ -1,10 +1,29 @@
-import NavBar from "@/components/ui/NavBar";
+import NavBar from '@/components/ui/NavBar';
+import SearchBarDrink from '@/components/ui/SearchBarDrink';
+import bgDrink from '../../public/imgs/drinkbg.jpg'
 
 function Drinks() {
   return (
-    <div className="bg-red-500 min-h-screen w-full">
-      <NavBar />
-      <h1 className="text-white text-4xl">Página Drinks</h1>
+    <div className="min-h-screen w-full">
+      <header 
+        className="bg-red-900 min-h-[400px] bg-cover bg-center bg-no-repeat relative flex flex-col"
+        style={{
+          backgroundImage: `url(${bgDrink})`
+        }}
+      >
+        <NavBar />
+        
+        {/* Container para centralizar o SearchBar */}
+        <div className="flex-1 flex items-end justify-center pb-8">
+          <SearchBarDrink />
+        </div>
+      </header>
+      
+      <main className='bg-white my-5'>
+        teste
+      </main>
+      
+      <footer>teste</footer>
     </div>
   );
 }
